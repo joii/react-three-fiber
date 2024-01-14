17e9cf050e7d7bcbe801267363e9604224680eee
phatchara yubon <contact.puipui@gmail.com>
2024-01-14 10:26:31 +0700

    Update custom and 3d file

--- src/Custom.js
+++ src/Custom.js
@@ -1,6 +1,6 @@
 import React,{ useRef, useState, useEffect } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { useGLTF, ContactShadows, Environment, OrbitControls } from "@react-three/drei"
+import { useGLTF, ContactShadows, Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { HexColorPicker } from "react-colorful"
 import { proxy, useSnapshot } from "valtio"
 import './styles.css'
@@ -15,20 +15,16 @@ const state = proxy({
 
 
 export default function Custom() {
-  const [color, setColor] = useState("#b32aa9");
-
-const presetColors = ["#cd9323", "#1a53d8", "#9a2151", "#0d6416", "#8d2808"];
-
 
     return (
        <div>
-       <Canvas shadows camera={{ position: [0, 0, 4], fov: 45 }}>
+       <Canvas shadows camera={{ position: [0, 0, 6], fov: 65 }} >
        <ambientLight intensity={0.7} />
        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
        <Shoe />
        <Environment preset="city" />
        <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
-       <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+       <OrbitControls minPolarAngle={Math.PI/2 } maxPolarAngle={Math.PI } enableZoom={true} enablePan={true} />
        </Canvas>
        <Picker/>
       
@@ -39,14 +35,14 @@ const presetColors = ["#cd9323", "#1a53d8", "#9a2151", "#0d6416", "#8d2808"];
   function Shoe() {
     const ref = useRef()
     const snap = useSnapshot(state)
-    const { nodes, materials } = useGLTF("glbf/shoe-draco.glb")
+    const { nodes, materials } = useGLTF("glbf/Casual_Shoes.glb")
     const [hovered, set] = useState(null)
   
-    useFrame((state) => {
-      const t = state.clock.getElapsedTime()
-      ref.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20)
-      ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
-    })
+    // useFrame((state) => {
+    //   const t = state.clock.getElapsedTime()
+    //   ref.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20)
+    //   ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+    // })
   
     useEffect(() => {
       const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="#fff-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
@@ -63,15 +59,90 @@ const presetColors = ["#cd9323", "#1a53d8", "#9a2151", "#0d6416", "#8d2808"];
         onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
         onPointerOut={(e) => e.intersections.length === 0 && set(null)}
         onPointerMissed={() => (state.current = null)}
-        onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
-        <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
-        <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
-        <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
-        <mesh receiveShadow castShadow geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
-        <mesh receiveShadow castShadow geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
-        <mesh receiveShadow castShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
-        <mesh receiveShadow castShadow geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
-        <mesh receiveShadow castShadow geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
+        onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}
+   
+        >
+        <PerspectiveCamera
+        makeDefault={false}
+        far={4.38}
+        near={0}
+        fov={34.52}
+        position={[4.11, 3.47, 10.83]}
+        rotation={[-0.31, 0.36, 0.11]}
+      />
+
+        <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_870.geometry}
+        material={materials.Chocolate_Chocolate_Leather_Chocolate}
+        material-color={snap.items.Chocolate_Chocolate_Leather_Chocolate}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_885.geometry}
+        material={materials.Chocolate_Chocolate_Leather_Chocolate}
+        material-color={snap.items.Chocolate_Chocolate_Leather_Chocolate}
+      />
+      <mesh
+      castShadow
+      receiveShadow
+      geometry={nodes.Mesh_871.geometry}
+      material={materials.Silk09}
+      material-color={snap.items.Silk09}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_886.geometry}
+        material={materials.Silk09}
+        material-color={snap.items.Silk09}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_884.geometry}
+        material={materials.Silk21}
+        material-color={snap.items.Silk21}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_874.geometry}
+        material={materials.Silk21}
+        material-color={snap.items.Silk21}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_1_1.geometry}
+        material={materials.Muted11}
+        material-color={snap.items.Muted11}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_1_2.geometry}
+        material={materials.Muted11}
+        material-color={snap.items.Muted11}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_3_1.geometry}
+        material={materials.Muted22}
+        material-color={snap.items.Muted22}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_3_2.geometry}
+        material={materials.Muted22}
+        material-color={snap.items.Muted22}
+      />
+      
+       
       </group>
     )
   }
